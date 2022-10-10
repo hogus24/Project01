2a2a6066d42f6b0a30fb6dfc93857bf30cf6a418
hogus24 <jangso4@naver.com>
2022-10-11 02:00:19 +0900

    GNB 재수정

--- js/gnb.js
+++ js/gnb.js
@@ -1,18 +1,25 @@
 $(function () {
-  var firstmenu = $(".gnb>li"),
+  var firstmenu = $(".gnb > li"),
     header_bottom = $(".header_nav"),
-    header_bottom_Height = header_bottom.height();
+    header_bottom_Height = header_bottom.height(),
+    header_top = $(".header_top");
 
   firstmenu.mouseenter(function () {
-    firstmenu.find("ul").hide();
     $(this).find("ul").show();
     var submenuHeight = $(this).find("ul").height();
     header_bottom
       .stop()
-      .animate({ height: submenuHeight + header_bottom_Height }, 500);
+      .animate({ height: submenuHeight + header_bottom_Height + 41 },0)
+      .addClass("gnbOn");
+    header_top.addClass("gnbOn");
   });
+
   firstmenu.mouseleave(function () {
-    header_bottom.stop().animate({ height: header_bottom_Height });
+    header_bottom
+      .stop()
+      .animate({ height: header_bottom_Height },0)
+      .removeClass("gnbOn");
     $(this).find("ul").hide();
+    header_top.removeClass("gnbOn");
   });
 });
