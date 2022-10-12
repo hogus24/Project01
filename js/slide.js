5b87300633e83acdc9daea89d4b9ffa9b12d0515
hogus24 <jangso4@naver.com>
2022-10-12 11:49:11 +0900

    슬라이드 구현

---
+++ js/slide.js
@@ -0,0 +1,24 @@
+$(function () {
+  var slideShow = $(".banner_slide");
+  var imagesSrc = ["Img_banner1.png", "Img_banner2.png", "Img_banner3.png"];
+  $.each(imagesSrc, function (a, o) {
+    var image = $("<img>");
+    image.attr("src", "img/" + o);
+    console.log(a, o);
+    slideShow.append(image);
+  });
+  slideShow.find("img").eq(0).addClass("current");
+
+  var j = 0;
+  setInterval(function () {
+    slideShow
+      .find("img")
+      .eq(j % imagesSrc.length)
+      .removeClass("current");
+    slideShow
+      .find("img")
+      .eq((j + 1) % imagesSrc.length)
+      .addClass("current");
+    j++;
+  }, 8000);
+});
